a108c9cccbdf3d5382dca7ba7fb70f5d56d9d769
Randy Wuchi <nnadoziennawuchi@yahoo.com>
2021-04-18 22:22:31 +0300

    added fade in and fade out effect to the overlay for the menu btn

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 const menuBtn = document.querySelector(".header__toggle");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("animate");
   overlay.classList.toggle("open");
+  body.classList.toggle("no-scroll");
+  if (overlay.classList.contains("open")) {
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  } else {
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  }
 });
